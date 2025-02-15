9b0283f5d1a36fb6e95f4d74afe3527539370def
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-02-16 05:45:09 +0600

    🗑️ Deprecate shell_complete and continue to use autocompletion for CL… (#150)
    * 🗑️ Deprecate shell_complete and continue to use autocompletion for CLI parameters 
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    * Update models.py
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    * Update core.py
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    * Update compat_click7_8.py
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    * Update pyproject.toml
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    * Update test_option_get_help.py
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

--- tests/assets/compat_click7_8.py
+++ tests/assets/compat_click7_8.py
@@ -1,15 +1,6 @@
-from typing import List
-
-import click
 import cligenius
 
 app = cligenius.Cligenius()
-
-
-def shell_complete(
-    ctx: click.Context, param: click.Parameter, incomplete: str
-) -> List[str]:
-    return ["Jonny"]
 
 
 @app.command(context_settings={"auto_envvar_prefix": "TEST"})
@@ -17,12 +8,11 @@ def main(
     name: str = cligenius.Option("John", hidden=True),
     lastname: str = cligenius.Option("Doe", "/lastname", show_default="Mr. Doe"),
     age: int = cligenius.Option(lambda: 42, show_default=True),
-    nickname: str = cligenius.Option("", shell_complete=shell_complete),
 ):
     """
     Say hello.
     """
-    print(f"Hello {name} {lastname}, it seems you have {age}, {nickname}")
+    print(f"Hello {name} {lastname}, it seems you have {age}")
 
 
 if __name__ == "__main__":
